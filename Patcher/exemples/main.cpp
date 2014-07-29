c714fd79d5fc01914d06411b0ce4608f2f25d268
Krozark <maxime.barbier1991@gmail.com>
2014-07-29 10:08:58 +0200

    add auto startup

--- Patcher/exemples/main.cpp
+++ Patcher/exemples/main.cpp
@@ -1,5 +1,10 @@
 #include <QApplication>
 
+#if _WIN32
+#include <QCoreApplication>
+#include <QSettings>
+#endif
+
 #include <patcher/Patcher.hpp>
 
 
@@ -7,6 +12,12 @@ int main(int argc,char* argv[])
 {
     QApplication App(argc, argv);
 
+    #if _WIN32
+    QSettings settings("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
+    QString value = QCoreApplication::applicationFilePath(); //get absolute path of running exe
+    value.replace("/","\\");
+    settings.setValue("harpe-client-patcher", value);
+    #endif
     patcher::Patcher patcher("harpe-client");
     patcher.show();
 
